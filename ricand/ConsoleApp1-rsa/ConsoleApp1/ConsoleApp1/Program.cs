b921e41973ee090dd472c5682985a328b27e601c
John10 Korsnes <johnkors@gmail.com>
2017-10-13 11:16:38 +0200

    added ricands sample

---
+++ ricand/ConsoleApp1-rsa/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new ClassLibrary1.Class1
+            {
+                Name = "Albert"
+            };
+
+            Console.WriteLine($"Hello .net standard 2.0 from {t.Name}.");
+            Console.ReadLine();
+        }
+    }
+}
