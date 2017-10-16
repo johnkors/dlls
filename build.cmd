7b90d35f7e6c1f919a5463ca589d0c9b0e8b6f72
john4ice <john.korsnes@ice.net>
2017-10-16 12:58:34 +0200

    Show output only

--- build.cmd
+++ build.cmd
@@ -1,12 +1,12 @@
-dotnet build "./source/dlls.sln" /v:quiet
 echo off
+dotnet build "./source/dlls.sln" /v:quiet
 echo *************
 echo Full framework project, new SDK style csproj:
 echo *************
-ls -hl source/console.fullframework.new/bin/debug/net461/*.dll
+ls source/console.fullframework.new/bin/debug/net461/*.dll | grep dll
 
 echo *************
 echo Full framework project, old csproj style:
 echo *************
-ls -hl source/console.fullframework.old/bin/debug/*.dll
+ls source/console.fullframework.old/bin/debug/*.dll | grep dll
 echo on
