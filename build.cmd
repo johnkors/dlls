f2a1331f1a597d969c01f3fd35e8e87acf035b3e
john4ice <john.korsnes@ice.net>
2017-10-16 12:42:37 +0200

    Print output folders content

--- build.cmd
+++ build.cmd
@@ -1,1 +1,12 @@
-dotnet build "./source/dlls.sln" /v:normal
+dotnet build "./source/dlls.sln" /v:quiet
+echo off
+echo *************
+echo Full framework project, new SDK style csproj:
+echo *************
+ls -hl source/console.fullframework.new/bin/debug/net461/*.dll
+
+echo *************
+echo Full framework project, old csproj style:
+echo *************
+ls -hl source/console.fullframework.old/bin/debug/*.dll
+echo on
