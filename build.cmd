ee09f5400cc6c4dc8bf9807e66d1e14f2d6b4403
john4ice <john.korsnes@ice.net>
2017-10-18 13:33:52 +0200

    update to netstandard2 and net471

--- build.cmd
+++ build.cmd
@@ -1,12 +1,25 @@
 echo off
 dotnet build "./source/dlls.sln" /v:quiet
+
+
 echo *************
-echo Full framework project, new SDK style csproj:
+echo Full framework project, new SDK style csproj (4.6.1):
 echo *************
 ls source/console.fullframework.new/bin/debug/net461/*.dll | grep dll
 
 echo *************
+echo Full framework project, new SDK style csproj (4.7):
+echo *************
+ls source/console.fullframework.new/bin/debug/net47/*.dll | grep dll
+
+echo *************
+echo Full framework project, new SDK style csproj (4.7.1):
+echo *************
+ls source/console.fullframework.new/bin/debug/net471/*.dll | grep dll
+
+echo  
+echo *************
 echo Full framework project, old csproj style:
 echo *************
 ls source/console.fullframework.old/bin/debug/*.dll | grep dll
-echo on
+
