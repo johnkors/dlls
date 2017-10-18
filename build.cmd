7c904f63ebdd7933abab10b97dfa4eb91abe29ab
john4ice <john.korsnes@ice.net>
2017-10-18 14:24:22 +0200

    Renamed since now multi-targeting in the new project

--- build.cmd
+++ build.cmd
@@ -1,21 +1,36 @@
 echo off
-dotnet build "./source/dlls.sln" /v:quiet
+dotnet build "./source/dlls.sln" /v:detailed
 
 
 echo *************
-echo Full framework project, new SDK style csproj (4.6.1):
+echo Full framework, new SDK style csproj (4.6.1):
 echo *************
-ls source/console.fullframework.new/bin/debug/net461/*.dll | grep dll
+ls source/console.new/bin/debug/net461/*.dll | grep dll
 
 echo *************
-echo Full framework project, new SDK style csproj (4.7):
+echo Full framework, new SDK style csproj (4.7):
 echo *************
-ls source/console.fullframework.new/bin/debug/net47/*.dll | grep dll
+ls source/console.new/bin/debug/net47/*.dll | grep dll
 
 echo *************
-echo Full framework project, new SDK style csproj (4.7.1):
+echo Full framework, new SDK style csproj (4.7.1):
 echo *************
-ls source/console.fullframework.new/bin/debug/net471/*.dll | grep dll
+ls source/console.new/bin/debug/net471/*.dll | grep dll
+
+echo *************
+echo NetCoreApp1.0, new SDK style csproj :
+echo *************
+ls source/console.new/bin/debug/netcoreapp1.0/*.dll | grep dll
+
+echo *************
+echo NetCoreApp1.1, new SDK style csproj :
+echo *************
+ls source/console.new/bin/debug/netcoreapp1.1/*.dll | grep dll
+
+echo *************
+echo NetCoreApp2.0, new SDK style csproj :
+echo *************
+ls source/console.new/bin/debug/netcoreapp2.0/*.dll | grep dll
 
 echo  
 echo *************
