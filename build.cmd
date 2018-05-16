0ff3ca1e00a48872e3db13489de0052929894621
John10 Korsnes <johnkors@gmail.com>
2018-05-16 21:48:02 +0200

    build.cmd update

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ echo ************************************
 
 echo ************************************
 echo Console apps using the new SDK:
-cat source/console.new/console.new.csproj
+cat source/console.sdk/console.sdk.csproj
 echo ************************************
 
 dotnet build "./source/dlls.sln" 
@@ -14,38 +14,45 @@ dotnet build "./source/dlls.sln"
 echo *************
 echo Full framework, new SDK style csproj (4.6.1):
 echo *************
-ls source/console.new/bin/debug/net461/*.dll | grep dll
+ls source/console.sdk/bin/debug/net461/*.dll | grep dll
 
 echo *************
 echo Full framework, new SDK style csproj (4.7):
 echo *************
-ls source/console.new/bin/debug/net47/*.dll | grep dll
+ls source/console.sdk/bin/debug/net47/*.dll | grep dll
 
 echo *************
 echo Full framework, new SDK style csproj (4.7.1):
 echo *************
-ls source/console.new/bin/debug/net471/*.dll | grep dll
+ls source/console.sdk/bin/debug/net471/*.dll | grep dll
 
 echo *************
 echo NetCoreApp1.0, new SDK style csproj :
 echo *************
-ls source/console.new/bin/debug/netcoreapp1.0/*.dll | grep dll
+ls source/console.sdk/bin/debug/netcoreapp1.0/*.dll | grep dll
 
 echo *************
 echo NetCoreApp1.1, new SDK style csproj :
 echo *************
-ls source/console.new/bin/debug/netcoreapp1.1/*.dll | grep dll
+ls source/console.sdk/bin/debug/netcoreapp1.1/*.dll | grep dll
 
 echo *************
 echo NetCoreApp2.0, new SDK style csproj :
 echo *************
-ls source/console.new/bin/debug/netcoreapp2.0/*.dll | grep dll
+ls source/console.sdk/bin/debug/netcoreapp2.0/*.dll | grep dll
 
 echo  
 echo *************
-echo 461 Full framework project , old csproj style:
+echo 461 Full framework project , old csproj style 12:
 echo *************
 ls source/console.fullframework.old.461.BuildTools12/bin/debug/*.dll | grep dll
+
+echo  
+echo *************
+echo 461 Full framework project , old csproj style 15:
+echo *************
+ls source/console.fullframework.old.461.BuildTools15/bin/debug/*.dll | grep dll
+
 
 echo  
 echo *************
